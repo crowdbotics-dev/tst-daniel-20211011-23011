0cb18c0e13086b72c01b74da6d6919d1cd9e5eac
Crowdbotics <engineering@crowdbotics.com>
2021-10-22 17:47:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class Car(models.Model):
     "Generated Model"
     num = models.BigIntegerField()
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="car_user",
+    )
